db631913aa1c328567a4bc27756bff09c736faf6
breakitfixit <163203848+breakitfixit@users.noreply.github.com>
2024-04-09 23:36:20 +0200

    loop-and-arrays afgerond

--- loops-and-arrays.js
+++ loops-and-arrays.js
@@ -1,10 +1,13 @@
 // ==========================================
 // Opdracht 1
-// Schrijf een script dat iedere naam in onderstaande array vervangt door een koosnaampje, door er -"je" achter te plakken.
+// Schrijf een script dat iedere naam in onderstaande array vervangt door een koosnaampje,
+// door er -"je" achter te plakken.
 // Zorg ervoor dat dit ook zou werken als de array wel 100 of 200 namen zou bevatten!
 // ==========================================
 
-const names = ["Henk", "Piet", "Fred", "Joop"];
+// const names = ["Henk", "Piet", "Fred", "Joop"];
+
+
 
 // Verwachtte uitkomsten:
 // Vóór het script zie je de originele waardes:
@@ -12,17 +15,19 @@ const names = ["Henk", "Piet", "Fred", "Joop"];
 // Na jouw script zie je de aangepaste waardes:
 // console.log(names) geeft: ["Henkje", "Pietje", "Fredje", "Joopje"]
 
-
-
+// for (let i = 0; i < names.length; i++) {
+//     console.log(names[i] + "je")
+// }
 
 
 // ==========================================
 // Opdracht 2
-// Schrijf een script dat ieder EVEN getal in onderstaande array met 2 vermenigvuldigd, en ieder ONEVEN getal met 3
+// Schrijf een script dat ieder EVEN getal in onderstaande array met 2 vermenigvuldigd,
+// en ieder ONEVEN getal met 3
 // Zorg ervoor dat dit ook zou werken als de array wel 100 of 200 getallen zou bevatten!
 // ==========================================
 
-const numbers = [2, 4, 5, 29, 38];
+// const numbers = [2, 4, 5, 29, 38];
 
 // Verwachtte uitkomsten:
 // Vóór het script zie je de originele waardes:
@@ -30,15 +35,28 @@ const numbers = [2, 4, 5, 29, 38];
 // Na jouw script zie je de aangepaste waardes:
 // console.log(numbers) geeft: [ 4, 8, 15, 87, 76 ];
 
+// for (let i = 0; i < numbers.length ; i++) {
+//     if (numbers[i] % 2 === 0) {
+//         // console.log(i * 2);
+//         numbers[i] *= 2;
+//     } else {
+//         numbers[i] *= 3;
+//     }
+// }
+//
+// console.log(numbers);
 
 
 
 
 // ==========================================
 // Opdracht 3
-// Schrijf een script dat voor de maat van ieder vierkant in onderstaande array, het volume uitrekent.
+// Schrijf een script dat voor de maat van ieder vierkant in onderstaande array,
+// het volume uitrekent.
 // Het volume van een vierkant is Lengte x Breedte x Hoogte
-// Vervolgens moet de huidige waarde in de array overschreven worden met: "Het volume van [a] is [b]"
+// Vervolgens moet de huidige waarde in de array overschreven worden met:
+// "Het volume van [a] is [b]"
+
 // Zorg ervoor dat dit ook zou werken als de array wel 100 of 200 getallen zou bevatten!
 // ==========================================
 
@@ -57,5 +75,9 @@ const squares = [30, 2, 8, 24, 11];
 //   'Het volume van 11 is 1331'
 // ]
 
+for (let i = 0; i < squares.length ; i++) {
+    const volume = squares[i] ** 3;
+    squares[i] = "Het volume van " + squares[i] + " is " + volume
+}
 
-
+console.log(squares)
